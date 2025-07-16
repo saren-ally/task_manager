e0f31e722478ba27a099d39304cb1fa70edeacfa
hezthuo <hezthuo@gmail.com>
2025-07-16 14:59:13 +0100

    Update routes.rb
    add crud endoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get "tasks/all_tasks", as: :all_tasks
   root "home#index"
-  get "/tasks/:id", to: "tasks#index"
+  get "tasks", to: "tasks#index", as: :tasks
+  get "tasks/new", to: "tasks#new", as: :new_task
+  get "/tasks/:id", to: "tasks#show", as: :task
+  post "tasks/find", to: "tasks#find", as: :find_task
+  post "tasks", to: "tasks#create", as: :create_task
+  delete "tasks/:id/delete", to: "tasks#delete", as: :task_delete
 end
