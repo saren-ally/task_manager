1d1710651e43c4dc51ce3dd43cf7c3b78240710c
hezthuo <hezthuo@gmail.com>
2025-07-16 23:46:16 +0100

    Update tasks_controller_test.rb
    add test "should return all tasks in the database"

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,8 +1,21 @@
 require "test_helper"
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @task = tasks(:one)
+  end
+
   test "should get all_task url" do
     get tasks_url
     assert_response :success
+  end
+
+  test "should return all tasks in the database" do
+    get tasks_url
+    assert_response :success
+
+    Task.all.each do |task|
+      assert_match task.title, response.body
+    end
   end
 end
