2873f25f27d0ffc8235d65b76f5175d1356fc5fd
hezthuo <hezthuo@gmail.com>
2025-07-16 15:30:27 +0100

    fix tasks url test

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
   test "should get all_task url" do
-    get all_tasks_url
+    get tasks_url
     assert_response :success
   end
 end
