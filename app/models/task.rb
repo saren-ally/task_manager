65249d0f768c979460857cb57ff71fb4abcf910f
hezthuo <hezthuo@gmail.com>
2025-07-17 22:44:01 +0100

    add validations
    add presence validation for due and description

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,5 @@
 class Task < ApplicationRecord
-  validates :title, presence: true
-  validates :status, presence: true
+  validates :title, :status, :due, :description, presence: true
 
   enum :status, { in_progress: 0, completed: 1 }
 end
