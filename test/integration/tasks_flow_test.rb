2f8c498d408452a5d497faccea8e2ad272092055
hezthuo <hezthuo@gmail.com>
2025-07-18 21:55:18 +0100

    fix test

--- test/integration/tasks_flow_test.rb
+++ test/integration/tasks_flow_test.rb
@@ -3,16 +3,17 @@
 require "test_helper"
 
 class TasksFlowTest < ActionDispatch::IntegrationTest
-  test "index should show 'Show' and 'delete' buttons with correct paths" do
-    task = tasks(:one)
+  test "index should show delete button with correct path and attributes" do
+  task = tasks(:one)
 
-    get tasks_url
-    assert_response :success
+  get tasks_url
+  assert_response :success
 
-    assert_select "a[href='#{task_path(task)}']", text: "Show"
-
-    assert_select "form[action='#{task_delete_path(task)}'][method='post']" do
-      assert_select "input[name='_method'][value='delete']", 1
-    end
+  # Assert the delete form is present
+  assert_select "form[action='#{task_delete_path(task)}'][method='post']" do
+    assert_select "input[name='_method'][value='delete']", 1
+    assert_select "input[type='submit'],button" # Ensure there's a submit mechanism
   end
+end
+
 end
