5a552966ae6f2059b1602d9277af2f5f112d113e
hezthuo <hezthuo@gmail.com>
2025-07-19 00:32:57 +0100

    add edit/update endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get "tasks", to: "tasks#index", as: :tasks
   get "tasks/new", to: "tasks#new", as: :new_task
   get "/tasks/:id", to: "tasks#show", as: :task
+  # edit
+  get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update", as: :update_task
   post "tasks/find", to: "tasks#find", as: :find_task
   post "tasks", to: "tasks#create", as: :create_task
   delete "tasks/:id/delete", to: "tasks#delete", as: :task_delete
