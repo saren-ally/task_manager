70b2b0d60bda59131c89122660f348be8c38206f
hezthuo <hezthuo@gmail.com>
2025-07-18 21:58:17 +0100

    fix linter moan

--- test/integration/tasks_flow_test.rb
+++ test/integration/tasks_flow_test.rb
@@ -4,16 +4,15 @@ require "test_helper"
 
 class TasksFlowTest < ActionDispatch::IntegrationTest
   test "index should show delete button with correct path and attributes" do
-  task = tasks(:one)
+    task = tasks(:one)
 
-  get tasks_url
-  assert_response :success
+    get tasks_url
+    assert_response :success
 
-  # Assert the delete form is present
-  assert_select "form[action='#{task_delete_path(task)}'][method='post']" do
-    assert_select "input[name='_method'][value='delete']", 1
-    assert_select "input[type='submit'],button" # Ensure there's a submit mechanism
+    # Assert the delete form is present
+    assert_select "form[action='#{task_delete_path(task)}'][method='post']" do
+      assert_select "input[name='_method'][value='delete']", 1
+      assert_select "input[type='submit'],button" # Ensure there's a submit mechanism
+    end
   end
-end
-
 end
